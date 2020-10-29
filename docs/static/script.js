df05db76bb70bff4d3ac42f9b66f4e270e35e70d
Kumassy <kumassyii@gmail.com>
2020-10-29 12:45:06 +0900

    fix: copy to clipboard

--- docs/static/script.js
+++ docs/static/script.js
@@ -1,5 +1,6 @@
 document.getElementById('copy').addEventListener('click', () => {
     const code = document.getElementById('code').textContent;
-    navigator.clipboard.writeText(code);
-    alert('コピーしました');
+    navigator.clipboard.writeText(code).then(() => {
+        alert('コピーしました');
+    })
 })
